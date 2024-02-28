69704c2f886f5687d2c575688268d6177af87a60
Abe Biswas <abe.biswas@saucelabs.com>
2024-02-28 03:30:11 -0800

    feat: Add hyperlinks to description

--- src/components/organisms/InputPanel.js
+++ src/components/organisms/InputPanel.js
@@ -9,7 +9,7 @@ class InputPanel extends Component {
           <div className="input-panel-text">
             <h1 className="input-panel-text__title">That’s SO YAML 👁🔮</h1>
             <h2 className="input-panel-text__subtitle">Sauce Orchestrate YAML Config Generator</h2>
-            <h3 className="input-panel-text__description">Sauce Orchestrate tests are kicked off via CLI utility `saucectl`. `saucectl` commands can be run manually or part of your CI/CD, and references a YAML file in your repo which instructs the utility how to run your tests. Use this YAML generator to create a custom YAML for your test suite.</h3>
+            <h3 className="input-panel-text__description">Sauce Orchestrate tests are kicked off via CLI utility <a href="https://docs.saucelabs.com/dev/cli/saucectl/">saucectl</a>. saucectl commands can be run manually or part of your CI/CD, and references a <a href="https://docs.saucelabs.com/orchestrate/saucectl-configuration/">YAML file</a> in your repo which instructs the utility how to run your tests. Use this YAML generator to create a custom YAML for your test suite.</h3>
           </div>
         </div>
       </div>
