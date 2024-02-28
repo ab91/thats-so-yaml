8327baf15f821ad5bca4adfe7f0f3727ac893bd2
Abe Biswas <abe.biswas@saucelabs.com>
2024-02-28 12:19:12 -0800

    feat: Lift state

--- src/components/molecules/InputMolecule.js
+++ src/components/molecules/InputMolecule.js
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
 
 class InputMolecule extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            topLevel: "sauce",
-            secondLevel: "tunnel",
-            thirdLevel: "name",
-            required: true,
-            documentation: "https://docs.saucelabs.com/dev/cli/saucectl/",
-            status: "✅/❌"
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
 
     render() {
         return (
             <div className="input-container input-border-gradient input-border-gradient--green">
-                <p className="input__title">{this.state.topLevel}
-                    {this.state.secondLevel && <span> → {this.state.secondLevel}</span>}
-                    {this.state.thirdLevel && <span> → {this.state.thirdLevel}</span>}
+                <p className="input__title">{this.props.topLevel}
+                    {this.props.secondLevel && <span> → {this.props.secondLevel}</span>}
+                    {this.props.thirdLevel && <span> → {this.props.thirdLevel}</span>}
                 </p>
-                <p className="input__subtitle">{this.state.required ? ("required") : ("optional")} - <a target="_blank" href={this.state.documentation} rel="noreferrer">docs</a></p>
+                <p className="input__subtitle">{this.props.required ? ("required") : ("optional")} - <a target="_blank" href={this.props.documentation} rel="noreferrer">docs</a></p>
                 <div className="input__data">placeholder</div>
-                <p className="input__status">{this.state.status}</p>
+                <p className="input__status">{this.props.status}</p>
             </div >
         );
     }
--- src/components/organisms/InputPanel.js
+++ src/components/organisms/InputPanel.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import InputMolecule from "../molecules/InputMolecule";
 
+/* Can be refactored to functional component */
+
+const Element = {
+  inputType: "radio",
+  topLevel: "sauce",
+  secondLevel: "tunnel",
+  thirdLevel: "name",
+  required: true,
+  documentation: "https://docs.saucelabs.com/dev/cli/saucectl/",
+  status: "✅/❌"
+};
+
 class InputPanel extends Component {
 
   render() {
@@ -13,7 +25,7 @@ class InputPanel extends Component {
             <h3 className="input-panel-text__description">Sauce Orchestrate tests are kicked off via CLI tool <a target="_blank" href="https://docs.saucelabs.com/dev/cli/saucectl/" rel="noreferrer">saucectl</a>. saucectl commands can be executed manually or automatically by your CI/CD system, and references a <a target="_blank" href="https://docs.saucelabs.com/orchestrate/saucectl-configuration/" rel="noreferrer">YAML file</a> in your repo which instructs Sauce Labs infrastructure how to run your tests. Use this app to interactively create a custom YAML for your test suite.</h3>
           </div>
           <div className="input-data-container">
-            <InputMolecule />
+            <InputMolecule {...Element} />
           </div>
         </div>
       </div>
